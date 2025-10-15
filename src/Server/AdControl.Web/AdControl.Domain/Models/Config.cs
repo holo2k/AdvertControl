87237a1cddfc5be3fd221852f68942ba034f272f
holo2k <nneketaa@yandex.ru>
2025-10-15 23:52:11 +0500

    feat (models) : edit config model

--- src/Server/AdControl.Web/AdControl.Domain/Models/Config.cs
+++ src/Server/AdControl.Web/AdControl.Domain/Models/Config.cs
@@ -5,5 +5,7 @@ public class Config
     public Guid Id { get; set; }
     public Guid? UserId { get; set; }
     public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public long Version { get; set; }
     public List<ConfigItem> Items { get; set; } = new();
 }
