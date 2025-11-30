31cc59129bafc30de7b2647c9953d387fc3592b9
younggogy <younggogy@gmail.com>
2025-11-30 21:44:45 +0500

    докинул миграции

---
+++ src/Server/AdControl.Core/Migrations/20251130162610_xz.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AdControl.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class xz : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TestCICD",
+                table: "Configs",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TestCICD",
+                table: "Configs");
+        }
+    }
+}
